a03558356745b4aca9b6d82f912fbf5eac021017
Erin <erindb@stanford.edu>
2015-10-19 13:15:41 -0700

    roll back to original uniqueturker

--- experiments/_shared/js/uniqueturker.js
+++ experiments/_shared/js/uniqueturker.js
@@ -11,36 +11,34 @@ function turkGetParam( name, defaultValue ) {
     } 
 }
 
-var turkParams = {};
-
-(function() {
-  var param = function(url, name ) {
-    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    var regexS = "[\\?&]"+name+"=([^&#]*)";
-    var regex = new RegExp( regexS );
-    var results = regex.exec( url );
-    return ( results == null ) ? "" : results[1];
-  }
-    
-    var url = window.location.href,
-      src = param(url, "assignmentId") ? url : document.referrer,
-      keys = ["assignmentId","hitId","workerId","turkSubmitTo"];
-  
-  keys.map(function(key) {
-    turkParams[key] = unescape(param(src, key));
-  });
-})();
-
 function UTWorkerLimitReached(ut_id, workerId, assignmentId) {
-    var assignmentId = assignmentId || turkParams['assignmentId'];
+    var assignmentId = assignmentId || turkGetParam('assignmentId', '');
     if (assignmentId != '' && assignmentId != 'ASSIGNMENT_ID_NOT_AVAILABLE') {
-        var workerId = workerId || turkParams['workerId'];
+        var workerId = workerId || turkGetParam('workerId', '');
         var url = '//uniqueturker.myleott.com/'+ut_id+'/'+workerId+'/'+assignmentId;
 
+        var response = turkGetParam('ut_response', '-1');
+        if (window.XDomainRequest) {
+            if (response == '-1') {
+                // Use Microsoft XDR
+                var xdr = new XDomainRequest();
+                xdr.open("get", url);
+                xdr.onload = function() {
+                    response = xdr.responseText;
+                    window.location.replace(window.location.href + "&ut_response="+response);
+                };
+                xdr.send();
+            }
+        } else {
             var request = new XMLHttpRequest();
             request.open('GET', url, false);
             request.send();
             response = request.responseText;
+        }
+
+        if (response == '0') {
+            return true;
+        }
     }
     return false;
 }
